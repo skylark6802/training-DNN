5b85f254c2d1e5e6fb9bb7073148135a2eca71b4
b99902060 <r03922057@ntu.edu.tw>
2015-04-01 16:03:30 +0800

    can run, but wrong

--- dnn.py
+++ dnn.py
@@ -4,6 +4,7 @@ import theano.tensor as tt
 
 x = tt.dscalar('x')
 y = tt.log(1 + tt.exp(x))
+#y = 1/(1+tt.exp(x))
 act = theano.function( [x], y )
 g = tt.grad(y, x)
 gact = theano.function( [x], g )
@@ -29,6 +30,9 @@ class HiddenLayer(object):
         self.params = [self.W, self.b]
     def run(self, input):
         self.output = tt.dot(input, self.W) + self.b 
+    def update(self, gW, gb, learning_rate):
+	self.W = tt.add(self.W ,-gW*learning_rate)
+	self.b = tt.add(self.b , -gb*learning_rate)
 # number of hidden layer can not be zero
 class deepNeuralNetwork(object):
     def __init__(self, num_hidden_layer, n_in, n_hidden, n_out):
@@ -72,9 +76,9 @@ class deepNeuralNetwork(object):
 	    self.OutputLayer.run([act(j) for j in self.hiddenLayer[self.num_hidden_layer-1].output.eval()[0]])
             output.append(self.OutputLayer.output.eval()[0])
             self.z.append(output)
-            inputs=[feature[i]]
+            inputs=[np.asarray(feature[i])]
             for out in output:
-                inputs.append([act(o) for o in out])
+                inputs.append(np.asarray([act(o) for o in out]))
             self.a.append(inputs)
 
     def calculate_error(self, y, index):
@@ -82,23 +86,24 @@ class deepNeuralNetwork(object):
         for i in range(len(index)):
             if not max(self.a[i][self.num_hidden_layer+2]) == y[index[i]]:
                 self.err += 1
-	print float(self.err)/len(index)
+	print 1-float(self.err)/len(index)
     
     def backpropagation(self, y, index):
         self.delta=[]#store in reverse order (delta[0] is actually delta_L)
         for i in range(len(index)):
             dlt=[]
-            if not max(self.a[i][self.num_hidden_layer+2]) == y[index[i]]:# update only when incorrect prediction
-                dlt.append(np.asarray([gact(j) for j in self.z[i][self.num_hidden_layer+1]],dtype=theano.config.floatX ))
-                dl = np.asarray([gact(j) for j in self.z[i][self.num_hidden_layer]],dtype =theano.config.floatX)*tt.dot(dlt[0],self.OutputLayer.W.transpose())
-                dlt.append(dl)
-                for j in range(self.num_hidden_layer):
-                    dl = np.asarray([gact(k) for k in self.z[i][self.num_hidden_layer-j-1]],dtype =theano.config.floatX)*tt.dot(dlt[j+1],self.hiddenLayer[self.num_hidden_layer-j-1].W.transpose())
-                    dlt.append(dl)
+	    g = self.function_gradient(y[index[i]],self.a[i][self.num_hidden_layer+2])
+	    print g[y[index[i]]]
+            dlt.append(np.asarray([gact(j) for j in self.z[i][self.num_hidden_layer+1]]*g,dtype=theano.config.floatX ))
+	    dl = np.asarray([gact(j) for j in self.z[i][self.num_hidden_layer]],dtype =theano.config.floatX)*tt.dot(dlt[0],self.OutputLayer.W.transpose())
+	    dlt.append(dl.eval())
+	    for j in range(self.num_hidden_layer):
+	        dl = np.asarray([gact(k) for k in self.z[i][self.num_hidden_layer-j-1]],dtype =theano.config.floatX)*tt.dot(dlt[j+1],self.hiddenLayer[self.num_hidden_layer-j-1].W.transpose())
+		dlt.append(dl.eval())
             self.delta.append(dlt)
             
     def update(self, index):
-        learning_rate = 0.0001
+        learning_rate = 1
         gW = []
         gb = []
         for i in range(len(index)):
@@ -106,13 +111,23 @@ class deepNeuralNetwork(object):
             gradb = []
             len_a = len(self.a[i])-1 #output doesn't caculate gradient
             for j in range(len_a):
-                gradient.append(tt.dot(self.delta[i][len_a-1-j].transpose(),self.a[i][j]))
+		#print "delta: "+str(np.size(self.delta[i][len_a-1-j]))
+		#print "a: "+str(np.size(self.a[i][j]))
+                gradient.append(self.a[i][j].reshape(len(self.a[i][j]),1)*self.delta[i][len_a-1-j])
                 gradb.append(self.delta[i][len_a-1-j])
-            gW = tt.add(gW,gradient)
-            gb = tt.add(gb,gradb)
-        gparam = gW + gb
-	gparam /= len(index)
-        tt.add(self.params,-learning_rate*gparam)
+		#print self.a[i][j].reshape(len(self.a[i][j]),1)*self.delta[i][len_a-1-j]
+	    if gW == []:
+		gW = gradient
+		gb = gradb
+	    else:
+        	gW = np.add(gW,gradient)
+        	gb = np.add(gb,gradb)
+        gW /= len(index)
+	gb /= len(index)
+	self.InputLayer.update(gW[0],gb[0],learning_rate)
+	for i in range(self.num_hidden_layer):
+		self.hiddenLayer[i].update(gW[i+1],gb[i+1],learning_rate)
+	self.OutputLayer.update(gW[self.num_hidden_layer+1],gb[self.num_hidden_layer+1],learning_rate)
     def predict(self, feature, label, index):
         y = []
         acc = 0
@@ -122,5 +137,12 @@ class deepNeuralNetwork(object):
                 acc += 1
         acc /= float(len(index))
         return y, acc
+    def function_gradient(self, y, a):
+        p = np.zeros(1943)
+        p[y] = 1.0
+	s = tt.sum(a)
+	a = a / (s.eval())
+	print p[y]-a[y],y
+	return (p-a)*2
         
 
