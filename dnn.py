665061be8ff40f35d146b1d52af3d19ca97ae6eb
b99902060 <r03922057@ntu.edu.tw>
2015-04-01 20:19:15 +0800

    seems right

--- dnn.py
+++ dnn.py
@@ -4,7 +4,7 @@ import theano.tensor as tt
 
 x = tt.dscalar('x')
 y = tt.log(1 + tt.exp(x))
-#y = 1/(1+tt.exp(x))
+#y = 1/(1+tt.exp(-x))
 act = theano.function( [x], y )
 g = tt.grad(y, x)
 gact = theano.function( [x], g )
@@ -14,8 +14,8 @@ class HiddenLayer(object):
         if W is None:
             W_values = np.asarray(
                     rng.uniform(
-                        low=-np.sqrt(6. / (n_in + n_out)),
-                        high=np.sqrt(6. / (n_in + n_out)),
+                        low=-np.sqrt(0.6 / (n_in + n_out)),
+                        high=np.sqrt(0.6 / (n_in + n_out)),
                         size=(n_in, n_out)
                         ),
                     dtype=theano.config.floatX
@@ -23,11 +23,10 @@ class HiddenLayer(object):
             W = theano.shared(value=W_values, name='W', borrow=True)
 
         if b is None:
-            b_values = np.zeros((1,n_out), dtype=theano.config.floatX)
+            b_values = np.asarray(rng.uniform(low=-np.sqrt(0.6 / (n_in + n_out)),high=np.sqrt(0.6 / (n_in + n_out)),size=(1,n_out)), dtype=theano.config.floatX)
             b = theano.shared(value=b_values, name='b', borrow=True)
         self.W = W
         self.b = b
-        self.params = [self.W, self.b]
     def run(self, input):
         self.output = tt.dot(input, self.W) + self.b 
     def update(self, gW, gb, learning_rate):
@@ -44,9 +43,7 @@ class deepNeuralNetwork(object):
                             n_out=n_hidden)
         #hidden layer
         self.hiddenLayer = []
-        h = HiddenLayer(rng=rng, n_in=n_hidden, n_out=n_hidden)
-        self.hiddenLayer.append(h)
-        for i in range(1,num_hidden_layer):
+        for i in range(num_hidden_layer):
             h = HiddenLayer(rng=rng, n_in=n_hidden, n_out=n_hidden)
             self.hiddenLayer.append(h)
         #output layer
@@ -55,10 +52,6 @@ class deepNeuralNetwork(object):
                             n_in=n_hidden,
                             n_out=n_out)
         #set dnn parameters
-        self.params = self.InputLayer.params
-        for i in range(num_hidden_layer):
-            self.params += self.hiddenLayer[i].params
-        self.params += self.OutputLayer.params
         self.num_hidden_layer = num_hidden_layer
     
     def forward(self, feature, index):
@@ -71,8 +64,8 @@ class deepNeuralNetwork(object):
 	    self.hiddenLayer[0].run( [act(j) for j in (self.InputLayer.output.eval()[0])])
             output.append(self.hiddenLayer[0].output.eval()[0])
             for j in range(1,self.num_hidden_layer):
-		self.hiddenLayer[i].run([act(k) for k in self.hiddenLayer[i-1].output.eval()[0]])
-                output.append(self.hiddenLayer[i].output.eval()[0])
+		self.hiddenLayer[j].run([act(k) for k in self.hiddenLayer[j-1].output.eval()[0]])
+                output.append(self.hiddenLayer[j].output.eval()[0])
 	    self.OutputLayer.run([act(j) for j in self.hiddenLayer[self.num_hidden_layer-1].output.eval()[0]])
             output.append(self.OutputLayer.output.eval()[0])
             self.z.append(output)
@@ -84,7 +77,8 @@ class deepNeuralNetwork(object):
     def calculate_error(self, y, index):
         self.err = 0
         for i in range(len(index)):
-            if not max(self.a[i][self.num_hidden_layer+2]) == y[index[i]]:
+	    #print np.argmax(self.a[i][self.num_hidden_layer+2]),self.a[i][self.num_hidden_layer+2][np.argmax(self.a[i][self.num_hidden_layer+2])],self.a[i][self.num_hidden_layer+2][y[index[i]]]
+            if not np.argmax(self.a[i][self.num_hidden_layer+2]) == y[index[i]]:
                 self.err += 1
 	print 1-float(self.err)/len(index)
     
@@ -93,17 +87,16 @@ class deepNeuralNetwork(object):
         for i in range(len(index)):
             dlt=[]
 	    g = self.function_gradient(y[index[i]],self.a[i][self.num_hidden_layer+2])
-	    print g[y[index[i]]]
             dlt.append(np.asarray([gact(j) for j in self.z[i][self.num_hidden_layer+1]]*g,dtype=theano.config.floatX ))
-	    dl = np.asarray([gact(j) for j in self.z[i][self.num_hidden_layer]],dtype =theano.config.floatX)*tt.dot(dlt[0],self.OutputLayer.W.transpose())
+	    dl = np.asarray([gact(j) for j in self.z[i][self.num_hidden_layer]],dtype=theano.config.floatX)*tt.dot(dlt[0],self.OutputLayer.W.transpose())
 	    dlt.append(dl.eval())
 	    for j in range(self.num_hidden_layer):
-	        dl = np.asarray([gact(k) for k in self.z[i][self.num_hidden_layer-j-1]],dtype =theano.config.floatX)*tt.dot(dlt[j+1],self.hiddenLayer[self.num_hidden_layer-j-1].W.transpose())
+	        dl = np.asarray([gact(k) for k in self.z[i][self.num_hidden_layer-j-1]],dtype=theano.config.floatX)*tt.dot(dlt[j+1],self.hiddenLayer[self.num_hidden_layer-j-1].W.transpose())
 		dlt.append(dl.eval())
             self.delta.append(dlt)
             
     def update(self, index):
-        learning_rate = 1
+        learning_rate = 0.01
         gW = []
         gb = []
         for i in range(len(index)):
@@ -122,18 +115,26 @@ class deepNeuralNetwork(object):
 	    else:
         	gW = np.add(gW,gradient)
         	gb = np.add(gb,gradb)
-        gW /= len(index)
-	gb /= len(index)
+        for i in range(len(gW)):
+		for j in range(len(gW[i])):
+			gW[i][j] = gW[i][j] / float(len(index))
+	for i in range(len(gb)):
+		for j in range(len(gb[i])):
+			gb[i][j] = gb[i][j] / float(len(index))
 	self.InputLayer.update(gW[0],gb[0],learning_rate)
 	for i in range(self.num_hidden_layer):
 		self.hiddenLayer[i].update(gW[i+1],gb[i+1],learning_rate)
+	#for j in range(10):
+	    #print self.OutputLayer.W.eval()[j][y[index[i]]], self.OutputLayer.W.eval()[j][y[index[i]]+1]
 	self.OutputLayer.update(gW[self.num_hidden_layer+1],gb[self.num_hidden_layer+1],learning_rate)
+	#for j in range(10):
+	#    print self.OutputLayer.W.eval()[j][y[index[i]]], self.OutputLayer.W.eval()[j][y[index[i]]+1]
     def predict(self, feature, label, index):
         y = []
         acc = 0
         for i in range(len(index)):
-            y.append(max(self.a[i][self.num_hidden_layer+2]))
-            if max(self.a[i][self.num_hidden_layer+2]) == label[i]:
+            y.append(np.argmax(self.a[i][self.num_hidden_layer+2]))
+            if np.argmax(self.a[i][self.num_hidden_layer+2]) == label[i]:
                 acc += 1
         acc /= float(len(index))
         return y, acc
@@ -142,7 +143,7 @@ class deepNeuralNetwork(object):
         p[y] = 1.0
 	s = tt.sum(a)
 	a = a / (s.eval())
-	print p[y]-a[y],y
-	return (p-a)*2
+	#print 2*(p[y]-a[y]),y,s.eval(),a[y]
+	return (p-a)*-2
         
 
--- training_dnn.py
+++ training_dnn.py
@@ -3,10 +3,10 @@ from modelTool import Save_Model
 import random
 from inputs import dataset
 
-MAX_EPOCH = 3
+MAX_EPOCH = 100000
 
 if __name__ == '__main__':
-    data = dataset('./mfcc/train.ark','./state_label/train.lab')
+    data = dataset('./mfcc/train_scale.ark','./state_label/train.lab')
     
     training_data,training_label,val_data,val_label = data.GetInput()
     print 'training size='+str(len(training_label))
@@ -14,27 +14,27 @@ if __name__ == '__main__':
     epoch = 0
     print 'data load'
     dnn = deepNeuralNetwork(1,39,10,1943)
+    batch = random.sample(range(len(training_label)),10)
     while epoch < MAX_EPOCH:
-        batch = random.sample(range(len(training_label)),10)
         dnn.forward(training_data, batch)
         dnn.backpropagation(training_label, batch)
         dnn.update(batch)
        
-        dnn.calculate_error(training_label, batch)
+        #dnn.calculate_error(training_label, batch)
 	epoch += 1
 	
-	#if epoch % 10000 == 0:		
-    	#	batch = random.sample(range(len(val_label)),10000)
-    	#	dnn.forward(val_data, batch)
-    	#	pre, acc = dnn.predict(val_data, val_label, batch)
-	#	print acc
-       # if epoch % 100000 == 0:
-        Save_Model(dnn,str(epoch)+'.model')
-        #if epoch % 1000 == 0:
-	#	print epoch
+	if epoch % 10000 == 0:		
+    		batch = random.sample(range(len(val_label)),10000)
+    		dnn.forward(val_data, batch)
+    		pre, acc = dnn.predict(val_data, val_label, batch)
+		print acc
+        if epoch % 10000 == 0:
+        	Save_Model(dnn,str(epoch)+'.model')
+        if epoch % 1000 == 0:
+		print epoch
     print 'validation data' 
-    #batch = range(len(val_label))
-    batch = random.sample(range(len(val_label)),10)
+    batch = range(len(val_label))
+    #batch = random.sample(range(len(val_label)),10)
     dnn.forward(val_data, batch)
     pre, acc = dnn.predict(val_data, val_label, batch)
     print acc
