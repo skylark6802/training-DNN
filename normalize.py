0996b2ed45b81346a73d1bf3b83b3b78289f383a
warchero <r03921083@ntu.edu.tw>
2015-04-08 17:04:33 +0800

    stronger than other noob codes

--- normalize.py
+++ normalize.py
@@ -1,7 +1,7 @@
 class Dataset:
-    def __init__(self):
+    def __init__(self,dim):
 	self.dlist = []
-	self.DIM = 39
+	self.DIM = dim
     
     def ReadInput(self,arkfile):
 	with open(arkfile, "r") as fark:
@@ -30,17 +30,56 @@ class Dataset:
 	    for lst in self.dlist:
 		lst[i] = low + (lst[i]-minn)/ratio*dist
     
+    def Merge(self,scope):
+	self.mergelist=[]
+	self.scope = scope
+	length = len(self.dlist)
+	for i in range(length):
+	    if i-scope < 0 or i+scope > length-1:
+		continue
+	    try:
+		del mlist[0:69]
+		mlist = mlist + self.dlist[i+scope][1:]
+		mlist.insert(0,self.dlist[i][0])
+	    except NameError:
+		mlist = [self.dlist[i][0]]
+		for j in range(-1*scope,scope):
+		    mlist = mlist + self.dlist[i+j][1:]
+	    self.mergelist.append(mlist)
+
+    def MWriteOutput(self,arkfile):
+	with open(arkfile,"w") as fark:
+	    for lst in self.mergelist:
+		fark.write("%s " % lst[0])
+		for num in lst[1:]:
+		    fark.write("%f " % num)
+		fark.write("\n")
+
     def WriteOutput(self,arkfile):
 	with open(arkfile,"w") as fark:
 	    for lst in self.dlist:
-		fark.write("%s " % lst[0])
-		for i in range(1,self.DIM+1):
-		    fark.write("%f " % lst[i])
+		for num in lst[1:]:
+		    fark.write("%f " % num)
 		fark.write("\n")
 		
 if __name__ == "__main__":
-    data = Dataset()
-    data.ReadInput("MLDS_HW1_RELEASE_v1/mfcc/tmp.ark")
+    """
+    6 arguments
+    1.dimension
+    2.input file
+    3.scale range
+    4.scope(if need merge)
+    5.scope output file(if need merge)
+    6.output file(no merge)
+    """
+    data = Dataset(69)
+    data.ReadInput("../MLDS_HW1_RELEASE_v1/fbank/test.ark")
     data.Normalize(-1.0,1.0)
+    
+    #if need to merge
+    data.Merge(4)
+    data.MWriteOutput("test_fbank_m_scale.ark")
+    
+    #else
     #data.WriteOutput("MLDS_HW1_RELEASE_v1/mfcc/test_scale.ark")
-    data.WriteOutput("tmp_scale.ark")
+    data.WriteOutput("test_fbank_scale.ark")
